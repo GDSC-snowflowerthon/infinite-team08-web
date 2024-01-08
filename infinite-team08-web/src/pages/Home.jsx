d9b82320b4989dddec2d238b727ea6960cc52e8b
kelly6226 <kelly6226@naver.com>
2024-01-08 17:44:10 +0900

    [DOCS] styled components 다운

---
+++ infinite-team08-web/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CameraComponent from "../components/CameraComponent";
+
+const Home = () => {
+
+}
+
+export default Home;
+
+const MainPageDiv = styled.div`
+  height: 100vh;
+  max-width: 460px;
+  margin: 20px auto;
+`;
