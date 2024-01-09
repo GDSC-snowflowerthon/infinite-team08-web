15a0b07453e615b7cd35842669b26f231c6f0d2d
Lazernes <dbsauddn1@gmail.com>
2024-01-10 03:00:21 +0900

    [FEAT] TTS 기능 추가

--- infinite-team08-web/src/pages/Analysis.js
+++ infinite-team08-web/src/pages/Analysis.js
@@ -1,9 +1,34 @@
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import dummy_1 from "../assets/img/dummy_1.jpg";
 import dummy_2 from "../assets/img/dummy_2.jpg";
 
+
+
 function Analysis() {
+  const [fileContent, setFileContent] = useState('');
+  const synthesis = window.speechSynthesis;
+  
+  useEffect(() => {
+    const fetchFileContent = async () => {
+      try {
+        const response = await fetch('../assets/text/dummy.txt');
+        const content = await response.text();
+        setFileContent(content);
+      } catch (error) {
+        console.error('Error fetching file:', error);
+      }
+    };
+
+    fetchFileContent();
+  }, []); // 빈 배열은 컴포넌트가 처음 마운트될 때만 실행되도록 합니다.
+
+  const speakText = () => {
+    const utterance = new SpeechSynthesisUtterance(fileContent);
+    synthesis.speak(utterance);
+  };
+
   const movePage = useNavigate();
 
   const handleClick = () => {
@@ -11,10 +36,13 @@ function Analysis() {
   }
 
   return (
-    <Main onClick={handleClick}>
+    <Main /*onClick={handleClick}*/>
         <Picture alt="dummy" src={dummy_1} />
         <Picture alt="dummy" src={dummy_2} />
         <p>처음으로 돌아가려면 화면을 클릭하세요</p>
+        <div>
+        <button onClick={speakText}>Speak Text from File</button>
+        </div>
     </Main>
   )
 }
